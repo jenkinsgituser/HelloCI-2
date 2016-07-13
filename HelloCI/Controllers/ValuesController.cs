8f759eeea608d90faf0498c1422e1ac8876f9213
Vincent Yang <vincent.yang@vedaleon.com>
2016-07-13 16:09:08 +1000

    change return values for api/values to HelloCI1/HelloCI2

--- HelloCI/Controllers/ValuesController.cs
+++ HelloCI/Controllers/ValuesController.cs
@@ -12,13 +12,13 @@ namespace HelloCI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "HelloCI1", "HelloCI2" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "HelloCI value";
+            return "HelloCI";
         }
 
         // POST api/values
